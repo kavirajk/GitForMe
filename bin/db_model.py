4d4ab932eb9a5764edeff83d4e2f61c78149220d
Kaviraj <kavirajkanagaraj@gmail.com>
2014-09-13 17:37:35 +0530

    working version

---
+++ bin/db_model.py
@@ -0,0 +1,29 @@
+import web
+import requests
+
+db=web.database(dbn='mysql',db='gitforme',user='root')
+
+def get_token(userid):
+    rows=db.select('users')
+
+    for row in rows:
+        return row.token
+
+def put_commits(commits):
+    for commit in commits:
+        db.insert('commits',sha=commit['sha'],
+                  userid=commit['commit']['committer']['email'],
+                  repo=str(commit['commit']['url']),
+                  message=commit['commit']['message'],
+                  time=commit['commit']['committer']['date'])
+#        print commit['sha']
+
+
+def get_commits():
+    rows=db.select('commits')
+    return rows
+
+def get_latest_time():
+    rows=db.select('commits',order='time DESC')
+    for row in rows:
+        return row.time
---
+++ bin/gitforme.py
@@ -0,0 +1,87 @@
+import web
+from db_model import *
+import requests
+import json
+
+urls=(
+    '/','Login',
+    '/Fetch','Fetch'
+)
+
+
+app=web.application(urls,globals())
+render=web.template.render("templates")
+
+class Login:
+
+    def GET(self):
+        return render.login()
+
+
+class Fetch:
+
+    def POST(self):
+        url="https://api.github.com/user/repos"
+        userid=web.input("userid")
+        password=web.input("password")
+
+        print userid,password
+
+        
+        token=get_token(userid)
+        token="token "+token
+        header={'Authorization':token}
+
+        # getting the user repos using OAuth token
+
+        repos=requests.get(url,headers=header)
+        r_names=[]
+        for repo in repos.json():
+            r_names.append(repo['name'].encode('ascii'))
+
+        # getting commit msg for all the repos
+
+        c_url="https://api.github.com/repos/"+userid
+        all_commits=[]
+
+        ### Testing only ###
+
+        r_names=['speakup']
+
+
+        time=get_latest_time()
+
+        
+        for r_name in r_names:
+            if time!=None:
+                r_url=c_url+"/"+r_name+"/commits?since="+time.isoformat()
+            else:
+                r_url=c_url+"/"+r_name+"/commits"
+                
+            commits=requests.get(r_url,headers=header).json()
+            all_commits.append(commits)
+
+        
+        #putting the commits into the db
+#        return all_commits[0][0]
+
+        put_commits(commits)
+
+
+        # fetching the updated data from db and rendering it
+
+        commits=get_commits()
+
+
+        return render.fetch(commits)
+        
+ 
+
+
+
+if __name__=='__main__':
+    app.run()
+
+    while(True):
+        web.seeother("/Fetch")
+        sleep(300)
